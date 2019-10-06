ba810d66d81097d3cdafd4e97b722fc89793c7bc
murashi <nsa.toshi.shi@gmail.com>
2019-10-07 07:11:49 +0900

    リセット出来るように修正

--- event_propagation/01/js/index.js
+++ event_propagation/01/js/index.js
@@ -1,6 +1,8 @@
 (function () {
 
 	let counter = 0;
+	let functions = [];
+	let functionForDocument;
 
 	document.addEventListener('DOMContentLoaded', function() {
 
@@ -10,27 +12,47 @@
 			for (let i = 0; i < boxes.length; i++) {
 				const isEnableUseCapture = $("[name=useCapture" + i + "]").prop("checked");
 				const isStopPropagation = $("[name=stopPropagation" + i + "]").prop("checked");
-				boxes[i].addEventListener('click', function(e) {
-					boxes[i].children[0].innerText = counter++;
+
+				functions[i] = (function(e) {
+					this.children[0].innerText = counter++;
 					if (isStopPropagation) {
 						e.stopPropagation();
 					}
-				}, isEnableUseCapture);
+				}).bind(boxes[i]);
+
+				boxes[i].addEventListener('click', functions[i], isEnableUseCapture);
 			}
 
 
 			const isEnableUseCapture = $("[name=useCapture-document]").prop("checked");
 
-			document.addEventListener('click', function(e) {
+			functionForDocument = function(e) {
 				// 対象外のクリックイベントはスキップ
 				if (e.target.localName !== 'button' || e.target.innerText !== 'Click!!') {
 					return;
 				}
 				$('#document-listener > span').text(counter++);
-			}, isEnableUseCapture);
+			};
+			document.addEventListener('click', functionForDocument, isEnableUseCapture);
 
 
 		});
+
+		$('#reset').click(function() {
+			reset();
+		});
 	});
+
+	function reset() {
+		counter = 0;
+		$('#document-listener > span').text('not fired');
+		document.removeEventListener('click', functionForDocument);
+		const boxes = document.querySelectorAll('.box');
+		for (let i = 0; i < boxes.length; i++) {
+			boxes[i].children[0].innerText = 'not fired';
+			boxes[i].removeEventListener('click', functions[i]);
+			functions[i] = null;
+		}
+	}
 
 })();
--- event_propagation/02/js/index.js
+++ event_propagation/02/js/index.js
@@ -1,6 +1,9 @@
 (function () {
 
 	let counter = 0;
+	let functions = [];
+	let functionForDocument;
+	let functionForWindow;
 
 	document.addEventListener('DOMContentLoaded', function() {
 
@@ -10,41 +13,55 @@
 			for (let i = 0; i < boxes.length; i++) {
 				const isEnableUseCapture = $("[name=useCapture" + i + "]").prop("checked");
 				const isStopPropagation = $("[name=stopPropagation" + i + "]").prop("checked");
-				boxes[i].addEventListener('scroll', function(e) {
-					boxes[i].children[0].innerText = counter++;
+
+				functions[i] = (function(e) {
+					this.children[0].innerText = counter++;
 					if (isStopPropagation) {
 						e.stopPropagation();
 					}
-				}, isEnableUseCapture);
+				}).bind(boxes[i]);
+
+				boxes[i].addEventListener('scroll', functions[i], isEnableUseCapture);
 			}
 
-
-			const isEnableUseCapture = $("[name=useCapture-document]").prop("checked");
-
-			document.addEventListener('scroll', function(e) {
+			functionForDocument = function(e) {
 				// 対象外のスクロールイベントはスキップ
 				if (e.target.nodeName === '#body') {
 					return;
 				}
 				$('#document-listener > span').text(counter++);
-			}, isEnableUseCapture);
-
-			const isEnableUseCaptureWindow = $("[name=useCapture-window]").prop("checked");
-
-			window.addEventListener('scroll', function(e) {
+			};
+			functionForWindow = function(e) {
 				// 対象外のスクロールイベントはスキップ
 				if (e.target.nodeName === '#body') {
 					return;
 				}
 				$('#window-listener > span').text(counter++);
-			}, isEnableUseCaptureWindow);
-
-			// const aa = document.querySelector(`#scroll-box`);
-			// aa.addEventListener('scroll', function(e) {
-			// 	console.dir(e);
-			// });
+			};
+			const isEnableUseCapture = $("[name=useCapture-document]").prop("checked");
+			document.addEventListener('scroll', functionForDocument, isEnableUseCapture);
+			const isEnableUseCaptureWindow = $("[name=useCapture-window]").prop("checked");
+			window.addEventListener('scroll', functionForWindow, isEnableUseCaptureWindow);
 
 		});
-	});
+
+		$('#reset').click(function() {
+			reset();
+		});
+    });
+
+	function reset() {
+		counter = 0;
+		$('#window-listener > span').text('not fired');
+		$('#document-listener > span').text('not fired');
+		document.removeEventListener('scroll', functionForDocument);
+		window.removeEventListener('scroll', functionForWindow);
+		const boxes = document.querySelectorAll('.box');
+		for (let i = 0; i < boxes.length; i++) {
+			boxes[i].children[0].innerText = 'not fired';
+			boxes[i].removeEventListener('scroll', functions[i]);
+			functions[i] = null;
+		}
+	}
 
 })();
